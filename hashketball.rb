a0e0c4f616224711b644505f96aba5fe076b3374
Abhinav Sharma <abhi.sharm.414@gmail.com>
2020-04-16 18:29:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,6 @@
-# Write your code here!
+def game_hash
+  {
+    :home => { :team_name => "Brooklyn Nets", :colors => ["Black", "White"], :players },
+    :away => { :team_name , :colors , :players }
+  }
+end
